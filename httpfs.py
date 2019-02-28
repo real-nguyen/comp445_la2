89a525bd6d2067a7ba41677b2d9f760323f21946
Real Nguyen <real.nguyen@hotmail.com>
2019-02-28 01:01:33 -0500

    - Added security check for get requests - Added status code dictionary - Renamed prepend_response to write_response_headers - Added status code parameter to write_response_headers - Removed socket parameter from print_debug_info

--- httpfs.py
+++ httpfs.py
@@ -27,8 +27,14 @@ REGEX_REQUEST = rf"^({VERB_GET}|{VERB_POST})\s+/([\w\./]+)*"
 
 # TODO: Programatically set value
 debug = True
+statuses = {
+    200: "OK",
+    400: "BAD REQUEST",
+    403: "FORBIDDEN",
+    404: "NOT FOUND"
+}
 
-def print_debug_info(clientsocket, address, data):
+def print_debug_info(address, data):
     if not data:
         print(f'[{datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S")} GMT] {address} end transmission.')
         return
@@ -40,9 +46,10 @@ def handle_request(clientsocket, request):
     match = re.search(REGEX_REQUEST, request)
     if match is None:
         if debug: 
-            print('Malformed request:')
-            print(f'{request}')
-        response_bytes = bytes('Malformed request:\r\n' + request, encoding='ASCII')
+            print('400 BAD REQUEST')
+            print(request)
+        response_str = write_response_headers(f'400 BAD REQUEST\r\n{request}\r\n\r\n', 400)
+        response_bytes = bytes(response_str, encoding='ASCII')
         conn.sendall(response_bytes)
         return
     verb = match.group(1)
@@ -53,11 +60,16 @@ def handle_request(clientsocket, request):
     if verb == VERB_POST:
         # TODO: Write regex/method to get request body
         #handle_post(directory, body)
-        return        
+        return
         
 def handle_get(clientsocket, path):
-    # TODO: Check that path is not outside server (application) root directory
-    # TODO: Handle 404
+    # Check that path is not outside server (application) root directory
+    if path is not None and DIRNAME.lower() not in os.path.abspath(path).lower():
+        response_str = write_response_headers('403 FORBIDDEN\r\n\r\n', 403)
+        response_bytes = bytes(response_str, encoding='ASCII')
+        clientsocket.sendall(response_bytes)
+        return
+
     if path is None or os.path.isdir(path):
         contents = os.listdir(path)
         response_str = ''
@@ -65,27 +77,35 @@ def handle_get(clientsocket, path):
             response_str += f + '\r\n'
         # Add another newline to denote end of response
         response_str += '\r\n'
-        response_str = prepend_response(response_str)
+        response_str = write_response_headers(response_str, 200)
         response_bytes = bytes(response_str, encoding='ASCII')
         clientsocket.sendall(response_bytes)
         return
     elif os.path.isfile(path):
         with open(path) as f:
             contents = f.read()
-            response_str = prepend_response(contents)
+            contents += '\r\n\r\n'
+            response_str = write_response_headers(contents, 200)
             response_bytes = bytes(response_str, encoding='ASCII')
             clientsocket.sendall(response_bytes)
             return
+    else:
+        if debug:
+            print('404 NOT FOUND')
+        response_str = write_response_headers('404 NOT FOUND\r\n\r\n', 404)
+        response_bytes = bytes(response_str, encoding='ASCII')
+        clientsocket.sendall(response_bytes)
+
 
 def handle_post(directory, body):
     return
 
-def prepend_response(response_str):
+def write_response_headers(response_str, status_code):
         # Prepend string with extra info for httpc's verbose flag
         response_str = f'Content-Length: {len(response_str)}\r\n\r\n{response_str}'
         response_str = f'Date: {datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S")} GMT\r\n{response_str}'
         response_str = f'Content-Type: text/plain\r\n{response_str}'
-        response_str = f'HTTP/1.0 200 OK\r\n{response_str}'
+        response_str = f'HTTP/1.0 {status_code} {statuses[status_code]}\r\n{response_str}'
         return response_str
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -97,7 +117,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             while True:
                 data = conn.recv(BUFFER_SIZE)
                 if debug:
-                    print_debug_info(conn, addr, data)
+                    print_debug_info(addr, data)
                 if not data:
                     # Going outside the loop will close the socket
                     break
