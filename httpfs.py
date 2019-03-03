705c13dfb0576cf3c97f68c512e3de37b5218442
Real Nguyen <real.nguyen@hotmail.com>
2019-03-02 22:28:34 -0500

    - Added option to change working directory - Added body for help method - Modified path regex - Added check for when OS does not grant permissions to create/modify files - Removed test files

--- httpfs.py
+++ httpfs.py
@@ -3,7 +3,9 @@ import re
 from sys import exit
 from datetime import datetime
 from os import listdir
+from os import chdir
 from os.path import abspath
+from os.path import join
 from os.path import isdir
 from os.path import isfile
 from os.path import dirname
@@ -28,8 +30,8 @@ REGEX_FLAGS = r"(?P<flag>-{1,2}\S*)(?:[=:]?|\s+)(?P<params>[^-\s].*?)?(?=\s+[-\/
 REGEX_URL = r"(http://)([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?"
 # Command regexes
 REGEX_REQUEST = rf"^({VERB_GET}|{VERB_POST})\s+/([\w\./]+)*"
-# Taken from https://www.regextester.com/104339
-REGEX_PATH = r"(\\\\?([^\\/]*[\\/])*)([^\\/]+)$"
+# Taken from https://www.regextester.com/96741
+REGEX_PATH = r"(^[a-zA-Z]:(\\|/)[(\\|/)\S|*\S]?.*$)"
 
 debug = False
 directory = DEFAULT_DIR
@@ -42,7 +44,11 @@ statuses = {
 }
 
 def help():
-    return
+    print(f'{APP_NAME} is a simple file server.')
+    print(f'Usage: {COMMAND_LISTEN} [{FLAG_DEBUG}] [{FLAG_PORT} PORT] [{FLAG_DIR_PATH} PATH-TO-DIR]')
+    print(f'\t{FLAG_DEBUG} Prints debugging messages.')
+    print(f'\t{FLAG_PORT} Specifies the port number that the server will listen and serve at. Default is {DEFAULT_PORT}.')
+    print(f'\t{FLAG_DIR_PATH} Specifies the directory that the server will use to read/write requested files. Default is the current directory when launching the application.')
 
 def print_debug_info(address, data):
     if not data:
@@ -119,6 +125,8 @@ def handle_get(clientsocket, path):
         send_response(clientsocket, response_str)
 
 def handle_post(clientsocket, path, body):
+    msg = ''
+    response_str = ''
     # Cannot write to file if no path (filename) in request
     # If path is a directory, then program needs explicit filename to write to
     if path is None or isdir(path):
@@ -155,17 +163,24 @@ def handle_post(clientsocket, path, body):
 
     # Write to file
     # Overwrite file if exists, otherwise create it
-    msg = ''
-    if isfile(path):
-        msg = f'Overwrote contents of {path} with:\r\n{body}'
-    else:
-        msg = f'File {path} created with contents:\r\n{body}'
-    with open(path, 'w+') as f:
-        f.write(body)
-    if debug:
-        print(get_status(200))
-        print(msg)
-    response_str = write_response_headers(msg + '\r\n\r\n', 200)
+    try:
+        if isfile(path):
+            msg = f'Overwrote contents of {path} with:\r\n{body}'
+        else:
+            msg = f'File {path} created with contents:\r\n{body}'
+        with open(path, 'w+') as f:
+            f.write(body)
+        if debug:
+            print(get_status(200))
+            print(msg)
+        response_str = write_response_headers(msg + '\r\n\r\n', 200)
+    except PermissionError:
+        status = get_status(403)
+        msg = 'You do not have permissions to modify the contents of this file or directory.'        
+        if debug:
+            print(status)
+            print(msg)
+        response_str = write_response_headers(f'{status}\r\n{msg}\r\n\r\n', 403)
     send_response(clientsocket, response_str)
 
 def write_response_headers(response_str, status_code):
@@ -203,16 +218,26 @@ def parse_query(query):
     if query == COMMAND_HELP:
         help()
         return False
+    if not query.startswith(COMMAND_LISTEN):
+        print(f"Unknown command. Type '{COMMAND_HELP}' for usage information.")
+        return False
     flags = get_flags(query)
     global debug, directory, port
     for flag, value in flags:
         if flag == FLAG_DEBUG:
             debug = True
-        # if flag == FLAG_DIR_PATH:
-        #     if not value:
-        #         print(f'Please enter a valid absolute path for flag {FLAG_DIR_PATH}.')
-        #         return False
-        #     directory = value
+        if flag == FLAG_DIR_PATH:
+            # Only tested for Windows
+            # Windows usually uses a backslash for its paths but is unintuitive to type in
+            path = value.strip("'").replace('/', '\\')            
+            if not re.search(REGEX_PATH, path):
+                print('Please enter a valid absolute path for the working directory.')
+                return False
+            if not isdir(path):
+                print('This path does not exist or is not a directory. Please create the directory or enter the absolute path of an existing directory.')
+                return False
+            chdir(path)
+            directory = path
         if flag == FLAG_PORT:
             if not value or not is_number(value) or not(0 < int(value) < 65535):
                 print('Please enter a positive number from 0 to 65535 for the port.') 
